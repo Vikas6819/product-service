721da2faab0a2aa4efad72a509581fe5bb43bdee
vikas.dhongade <vikas.dhongade@nextgl.com>
2022-09-17 17:07:29 +0530

    Created Project Structure

---
+++ product-core/src/main/java/com/javahouse/product/core/entities/Product.java
@@ -0,0 +1,8 @@
+package com.javahouse.product.core.entities;
+
+public class Product
+{
+  private Long productId;
+  private String productName;
+  private String productAmount;
+}
---
+++ product-web/src/main/java/controller/ProductController.java
@@ -0,0 +1,8 @@
+package controller;
+
+import com.javahouse.product.core.entities.Product;
+
+public class ProductController
+{
+  private Product product;
+}
